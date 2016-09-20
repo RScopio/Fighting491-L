9dbbe7c82d1450f250fd67b3bb7c951994fa54cc
Ryan Scopio <ryanscopio@gmail.com>
2016-09-20 00:11:08 -0700

    Added my own spritesheet Creating a default controlelr script that is a mix between Yema's and my code Added Walking left & right

---
+++ Assets/Scripts/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator), typeof(SpriteRenderer), typeof(Rigidbody2D))]
+public class DefaultController : MonoBehaviour
+{
+    private enum AnimState
+    {
+        idle,
+        walk
+    };
+
+    public float Speed = 1;
+
+    Animator anim;
+    SpriteRenderer render;
+    Rigidbody2D body;
+    AnimState state = AnimState.idle;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        render = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Movement()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        if (horiz > 0)
+        {
+            transform.Translate(Vector2.right * Speed * Time.deltaTime);
+            render.flipX = false;
+        }
+
+        if (horiz < 0)
+        {
+            transform.Translate(Vector2.left * Speed * Time.deltaTime);
+            render.flipX = true;
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(horiz));
+    }
+
+    void Update()
+    {
+        Movement();
+        //check other actions
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            anim.SetBool("Ground", true);
+        }
+    }
+}
