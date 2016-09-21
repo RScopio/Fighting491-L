4b5e4e8eca8baa372d3175ac84e96ef1f2e04b13
Ryan Scopio <ryanscopio@gmail.com>
2016-09-21 13:02:11 -0700

    Edited controls so switching states has instant transition

--- Assets/Scripts/DefaultController.cs
+++ Assets/Scripts/DefaultController.cs
@@ -7,10 +7,18 @@ public class DefaultController : MonoBehaviour
     private enum AnimState
     {
         idle,
-        walk
+        walk,
+        crouch,
+        punch,
+        kick,
+        block
     };
 
     public float Speed = 1;
+    bool crouch = false;
+    bool ground = false;
+    float horiz = 0;
+    float vert = 0;
 
     Animator anim;
     SpriteRenderer render;
@@ -26,7 +34,49 @@ public class DefaultController : MonoBehaviour
 
     void Movement()
     {
-        float horiz = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horiz = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            horiz = -1;
+        }
+        else
+        {
+            horiz = 0;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vert = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            vert = -1;
+        }
+        else
+        {
+            vert = 0;
+        }
+
+
+        if (vert < 0 && ground)
+        {
+            crouch = true;
+            anim.SetBool("Crouch", true);
+        }
+        else
+        {
+            crouch = false;
+        }
+
+        if (vert > 0 && ground)
+        {
+            //jump
+            ground = false;
+        }
+
         if (horiz > 0)
         {
             transform.Translate(Vector2.right * Speed * Time.deltaTime);
@@ -39,6 +89,8 @@ public class DefaultController : MonoBehaviour
             render.flipX = true;
         }
 
+
+
         anim.SetFloat("Speed", Mathf.Abs(horiz));
     }
 
@@ -46,13 +98,17 @@ public class DefaultController : MonoBehaviour
     {
         Movement();
         //check other actions
+
+        anim.SetBool("Crouch", crouch);
+        anim.SetBool("Ground", ground);
+
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-            anim.SetBool("Ground", true);
+            ground = true;
         }
     }
 }
