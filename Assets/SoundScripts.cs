997112764b3190a44ccd421a95fbd8f041dc2214
KTPC\KT <kyungtyu@hotmail.com>
2016-10-03 13:38:01 -0700

    Options Menu added

---
+++ Assets/SoundScripts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundScripts : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+    public void VolumeController()
+    {
+        volumeAudio.volume = volumeSlider.value;
+    }
+}
