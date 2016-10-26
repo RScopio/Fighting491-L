4bb676e5f96e8028ce4242433a7267c80c9d89bd
Ryan Scopio <ryanscopio@gmail.com>
2016-10-26 02:07:17 -0700

    Added Xbox Controls Controls= Attack: X Power: Y Block: Right Bumper Jump: A or Left Stick Up Movement: Left or Right on Left Stick Crouch: Down on Left Stick

--- Assets/Scripts/DefaultController.cs
+++ Assets/Scripts/DefaultController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -50,6 +51,31 @@ public class DefaultController : MonoBehaviour
     //Vector3 movement
     //bool for states
 
+    //controller input
+    float xbox_hAxis;
+    float xbox_vAxis;
+    //float xbox_aAxis;
+    float xbox_htAxis;
+    float xbox_vtAxis;
+
+    float xbox_ltaxis;
+    float xbox_rtaxis;
+    float xbox_dhaxis;
+    float xbox_dvaxis;
+
+    bool xbox_a;
+    bool xbox_b;
+    bool xbox_x;
+    bool xbox_y;
+    bool xbox_lb;
+    bool xbox_rb;
+    bool xbox_ls;
+    bool xbox_rs;
+    bool xbox_back;
+    bool xbox_start;
+
+    Text DebugText;
+
     public AudioClip attackSound;
     //public AudioClip kickSound;
     public AudioClip jumpSound;
@@ -59,11 +85,54 @@ public class DefaultController : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+        DebugText = GameObject.FindGameObjectWithTag("DebugText").GetComponent<Text>();
+    }
+
+    void ReadInput()
+    {
+        xbox_hAxis = Input.GetAxis("Horizontal");
+        xbox_vAxis = Input.GetAxis("Vertical");
+        //xbox_aAxis = Input.GetAxis("Altitude");
+        xbox_htAxis = Input.GetAxis("HorizontalTurn");
+        xbox_vtAxis = Input.GetAxis("VerticalTurn");
+
+        xbox_ltaxis = Input.GetAxis("XboxLeftTrigger");
+        xbox_rtaxis = Input.GetAxis("XboxRightTrigger");
+        xbox_dhaxis = Input.GetAxis("XboxDpadHorizontal"); //not working
+        xbox_dvaxis = Input.GetAxis("XboxDpadVertical"); //not working
+
+        xbox_a = Input.GetButton("XboxA");
+        xbox_b = Input.GetButton("XboxB");
+        xbox_x = Input.GetButton("XboxX");
+        xbox_y = Input.GetButton("XboxY");
+        xbox_lb = Input.GetButton("XboxLB");
+        xbox_rb = Input.GetButton("XboxRB");
+        xbox_ls = Input.GetButton("XboxLS");
+        xbox_rs = Input.GetButton("XboxRS");
+        xbox_back = Input.GetButton("XboxBack");
+        xbox_start = Input.GetButton("XboxStart");
+
+        DebugText.text =
+        string.Format(
+            "Horizontal: {14:0.000} Vertical: {15:0.000}\n" +
+            "HorizontalTurn: {16:0.000} VerticalTurn: {17:0.000}\n" +
+            "LTrigger: {0:0.000} RTrigger: {1:0.000}\n" +
+            "A: {2} B: {3} X: {4} Y:{5}\n" +
+            "LB: {6} RB: {7} LS: {8} RS:{9}\n" +
+            "View: {10} Menu: {11}\n" +
+            "Dpad-H: {12:0.000} Dpad-V: {13:0.000}\n",
+            xbox_ltaxis, xbox_rtaxis,
+            xbox_a, xbox_b, xbox_x, xbox_y,
+            xbox_lb, xbox_rb, xbox_ls, xbox_rs,
+            xbox_back, xbox_start,
+            xbox_dhaxis, xbox_dvaxis,
+            xbox_hAxis, xbox_vAxis,
+            xbox_htAxis, xbox_vtAxis);
     }
 
     void Attack()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) || xbox_x)
         {
             attack = true;
             if (SoundManager.instance != null)
@@ -76,7 +145,7 @@ public class DefaultController : MonoBehaviour
             attack = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) || xbox_y)
         {
             power = true;
             if (SoundManager.instance != null)
@@ -89,7 +158,7 @@ public class DefaultController : MonoBehaviour
             power = false;
         }
 
-        if(Input.GetKey(KeyCode.C))
+        if (Input.GetKey(KeyCode.C) || xbox_rb)
         {
             block = true;
         }
@@ -120,12 +189,12 @@ public class DefaultController : MonoBehaviour
     void Movement()
     {
         #region Horizontal Input
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || xbox_hAxis > 0)
         {
             horiz = 1;
             direct = Direction.right;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow) || xbox_hAxis < 0)
         {
             horiz = -1;
             direct = Direction.left;
@@ -137,11 +206,11 @@ public class DefaultController : MonoBehaviour
         #endregion
 
         #region Vertical Input
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || xbox_a || xbox_vAxis > 0)
         {
             vert = 1;
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow) || xbox_vAxis < 0)
         {
             vert = -1;
         }
@@ -192,6 +261,7 @@ public class DefaultController : MonoBehaviour
 
     void Update()
     {
+        ReadInput();
         Attack();
         UpdateDirection();
         OnGroundCheck();
