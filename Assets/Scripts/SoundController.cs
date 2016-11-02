9906486c4c73f3616ede45e9ae6d83617b40c6d1
Ryan Scopio <ryanscopio@gmail.com>
2016-11-02 13:44:19 -0700

    Input & Sound Controller Scripts Added Deconstructed Default Controller into 3 scripts. 1 to handle input, 1 to handle sound, 1 to control character You can change the controls for the character in the unity editor now

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour
+{
+
+    public AudioClip AttackSound;
+    //public AudioClip kickSound;
+    public AudioClip JumpSound;
+
+    public void Attack()
+    {
+        if (SoundManager.instance != null)
+        {
+            SoundManager.instance.PlaySingle(AttackSound);
+        }
+    }
+
+    public void Power()
+    {
+        if (SoundManager.instance != null)
+        {
+            SoundManager.instance.PlaySingle(AttackSound);
+        }
+    }
+
+    public void Jump()
+    {
+        if (SoundManager.instance != null)
+        {
+            SoundManager.instance.PlaySingle(JumpSound);
+        }
+    }
+}
