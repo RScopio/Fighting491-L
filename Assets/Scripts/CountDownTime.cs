83e48a6d7bf64fd2e02265f530903bb119706a1e
ye <ye.ma.32@my.csun.edu>
2016-11-06 21:43:32 -0800

    add stage and countdown time in the testbattle scene

---
+++ Assets/Scripts/CountDownTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CountDownTime : MonoBehaviour {
+	public float RemainTime;
+	public Text counter;
+	// Use this for initialization
+	void Start () {
+		counter = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RemainTime -= Time.deltaTime;
+		counter.text =""+Mathf.Round(RemainTime);
+	}
+}
