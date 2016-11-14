94276642564848f59f844e961bd047270a55146f
Ryan Scopio <ryanscopio@gmail.com>
2016-11-14 13:29:16 -0800

    Added Health Bar Created Damage Collidor for Punch Created Damage Script You can now KILL EACHOTHER

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour
+{
+
+    public float DamageValue = 10f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Health enemy = collision.transform.GetComponent<Health>();
+        if (collision.transform.root != transform.root && enemy)
+        {
+            enemy.CurrentHealth -= DamageValue;
+        }
+    }
+}
