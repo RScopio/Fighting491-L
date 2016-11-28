3ce9ee9c9a7018e9f104d0b4076231375bcfdeec
ye <ye.ma.32@my.csun.edu>
2016-11-27 22:16:21 -0800

    Add round 1 animation and fix the countdown time in the the TestBattle

--- Assets/Scripts/CountDownTime.cs
+++ Assets/Scripts/CountDownTime.cs
@@ -4,14 +4,32 @@ using UnityEngine.UI;
 public class CountDownTime : MonoBehaviour {
 	public float RemainTime;
 	public Text counter;
+	public bool waittime;
 	// Use this for initialization
 	void Start () {
 		counter = GetComponent<Text> ();
+
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		RemainTime -= Time.deltaTime;
-		counter.text =""+Mathf.Round(RemainTime);
+		
+		if (RemainTime > 99) {
+			
+			RemainTime -= Time.deltaTime;
+			waittime = false;
+		} else if (RemainTime <= 99 && RemainTime > 0) {
+			waittime = true;
+			RemainTime -= Time.deltaTime;
+		} else {
+		
+			RemainTime = 0;
+		}
+		if (waittime==true) {
+			counter.text = "" + Mathf.Round (RemainTime);
+		}
+
 	}
 }
