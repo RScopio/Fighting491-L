c4d8ff872ee29b4bebdea5d49c08df0b1d017067
Ryan Scopio <ryanscopio@gmail.com>
2017-02-06 14:06:58 -0800

    Added a GameController object with a singleton script attached on the title screen. Also attached multiple scripts to help manage the game attached to the GameController. I didn't see the one that was already built in the character select, whoops. Next time I'll work on merging ours together

---
+++ Assets/Scripts/SimpleSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSingleton : MonoBehaviour {
+
+    private static SimpleSingleton instance = null;
+
+    public static SimpleSingleton Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        Debug.Log("SINGLETON UPDATE");
+    }
+}
